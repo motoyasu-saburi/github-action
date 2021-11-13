54487de9bdf733df07f6a680677a8c8c7ef0443d
motoyasu-saburi <kumagoro_alice@yahoo.co.jp>
2021-11-13 18:50:03 +0900

    test

---
+++ src/mul.py
@@ -0,0 +1,5 @@
+from math import pow
+
+def multiple(x) -> float:
+    return pow(x, 2)
+
---
+++ tests/test_mul.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+from src.mul import multiple
+
+
+class TestMul(TestCase):
+
+    def test_mul(self):
+        self.assertEqual(multiple(2), 4)
+
+    def test_mul2(self):
+        self.assertEqual(multiple(2), 2)
